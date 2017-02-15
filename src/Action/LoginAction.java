26260efdfb948c647613906dd1eeed50ab52b0fc
wenguofu <wgfu@i9i8.com>
2017-02-15 21:48:25 +0800

    no message

---
+++ src/Action/LoginAction.java
@@ -0,0 +1,10 @@
+package Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+public class LoginAction extends ActionSupport{
+public String execute()throws Exception
+{
+	return "success";
+	}
+}
+
