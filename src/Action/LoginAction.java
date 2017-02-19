9844ab359f3fdb7932109f85b0867bb3f94e4a24
wenguofu <wgfu@i9i8.com>
2017-02-19 15:40:27 +0800

    no message

--- src/Action/LoginAction.java
+++ src/Action/LoginAction.java
@@ -1,7 +1,13 @@
 package Action;
 
 import com.opensymphony.xwork2.ActionSupport;
+
 public class LoginAction extends ActionSupport{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 public String execute()throws Exception
 {
 	return "success";
---
+++ src/Action/RegistAction.java
@@ -0,0 +1,10 @@
+package Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegistAction extends ActionSupport{
+public String execute() throws Exception
+{
+	return "success";
+	}
+}
