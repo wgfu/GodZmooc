aba6f5392eb0d484db63a4fe1d44428354cbe3cf
wenguofu <wgfu@i9i8.com>
2017-02-25 13:53:19 +0800

    。。

--- src/Service/ScoreManageImp.java
+++ src/Service/ScoreManageImp.java
@@ -1,33 +1,42 @@
 package Service;
 
+import javax.annotation.Resource;
 
-
-
+import Dao.ITestAndHomeworkDao;
 
 public class ScoreManageImp implements ITestAndHomeworkManage{
+@Resource
+private ITestAndHomeworkDao iScoreDao;
+	public ITestAndHomeworkDao getIScoreDao() {
+	return iScoreDao;
+}
+
+public void setIScoreDao(ITestAndHomeworkDao iScoreDao) {
+	this.iScoreDao = iScoreDao;
+}
 
 	@Override
 	public <T> T getRuslt(Class<T> t) {
 		// TODO Auto-generated method stub
-		return null;
+		return iScoreDao.getRuslt(t);
 	}
 
 	@Override
 	public <T> void updateT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iScoreDao.updateT(t);
 	}
 
 	@Override
 	public <T> void deleteT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iScoreDao.deleteT(t);
 	}
 
 	@Override
 	public <T> void addT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iScoreDao.addT(t);
 	}
 	
 
--- src/Service/TestManageImp.java
+++ src/Service/TestManageImp.java
@@ -1,29 +1,42 @@
 package Service;
 
+import javax.annotation.Resource;
+
+import Dao.ITestAndHomeworkDao;
+
 public class TestManageImp implements ITestAndHomeworkManage{
+@Resource
+private ITestAndHomeworkDao iTestDao;
+	public ITestAndHomeworkDao getITestDao() {
+	return iTestDao;
+}
+
+public void setITestDao(ITestAndHomeworkDao iTestDao) {
+	this.iTestDao = iTestDao;
+}
 
 	@Override
 	public <T> T getRuslt(Class<T> t) {
 		// TODO Auto-generated method stub
-		return null;
+		return iTestDao.getRuslt(t);
 	}
 
 	@Override
 	public <T> void updateT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iTestDao.updateT(t);
 	}
 
 	@Override
 	public <T> void deleteT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iTestDao.deleteT(t);
 	}
 
 	@Override
 	public <T> void addT(Class<T> t) {
 		// TODO Auto-generated method stub
-		
+		iTestDao.addT(t);
 	}
 
 }
